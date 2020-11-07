1280052955a59a086418be855a073568392bb388
ido30902 <idobarabi@gmail.com>
2020-11-07 10:51:04 +0200

    added create store

---
+++ create_store.py
@@ -0,0 +1,15 @@
+import os
+
+def main():
+    store_name = str(input("Enter your new store name: "))
+
+    new_path = os.getcwd() + "/stores/"
+
+    os.mkdir(new_path + store_name)
+
+    html_file = open(new_path + store_name + "/index.html", "w")
+    html_file.write("<!DOCTYPE html><html><head></head><body></body></html>")
+    html_file.close()
+
+if __name__ == "__main__":
+    main()
